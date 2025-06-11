2d6398396aaecc10bfdf5ff29e95cf9e5d50b84e
Jasmina888Dev <jasminajumabaeva888@gmail.com>
2025-06-11 19:03:37 +0600

    jasminaaaa

--- src/components/Pages/University/University.jsx
+++ src/components/Pages/University/University.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import inti from "../../../assets/img/inti.svg";
 
 const University = () => {
   const [univer, setUniver] = useState([]);
 
   async function addUniver() {
-    let res = await axios('http://13.61.25.99/en/university_event/');
+    let res = await axios("http://13.61.25.99/en/university_event/");
     setUniver(res.data);
   }
 
@@ -15,9 +16,17 @@ const University = () => {
     addUniver();
   }, []);
   return (
-    <div id="univer">
+    <div id="UniversityDetails--bottom__Description">
       <div className="container">
-        <div className="univer"></div>
+        <div className="UniversityDetails--bottom__Description">
+          <div className="UniversityDetails--bottom__Description--inti">
+            <div className="UniversityDetails--bottom__Description--inti__text1">
+              <img src={inti} alt="img" />
+              <h1>INTI International University</h1>
+            </div>
+          </div>
+          <div className="UniversityDetails--bottom__Description--apply"></div>
+        </div>
       </div>
     </div>
   );
