d5f8fe34b8a2fd65effc455933b13be94e7a8fb5
Jasmina888Dev <jasminajumabaeva888@gmail.com>
2025-06-14 12:36:48 +0600

    jasmin univer1

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import AboutUs from './AboutUs';
-import Team from './Team';
+import React from "react";
+import AboutUs from "./AboutUs";
+import Team from "./Team";
 
 const About = () => {
   return (
     <section id="about">
-        <div className="container">
-            <div className="about">
-               <AboutUs/>
-               <Team/>
-            </div>
+      <div className="container">
+        <div className="about">
+          <AboutUs />
+          <Team />
         </div>
+      </div>
     </section>
   );
 };
